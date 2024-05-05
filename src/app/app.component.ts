ee8fe6df272f153cb050370b85847d9e5afaebc7
truongnx <xuantruong0944@gmail.com>
2024-05-05 16:34:13 +0700

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'admin-system';
+  /**
+   *
+   */
+  constructor(private auth: AuthService) {
+
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.isUserOnl()
+  }
+  // user Onl
+  isUserOnl() {
+    // Thiết lập hàm định thời để gọi lại sau mỗi 5 phút
+    if (this.auth.isUserLoggedIn()) {
+      this.auth.isUserOnl().subscribe(response => {
+        console.log('user-onl');
+      }, error => {
+        sessionStorage.removeItem('token');
+        window.location.reload();
+      });
+      setInterval(() => {
+        if (this.auth.isUserLoggedIn()) {
+          this.auth.isUserOnl().subscribe(response => {
+            console.log('user-onl');
+          }, error => {
+            sessionStorage.removeItem('token');
+            window.location.reload();
+          });
+        }
+      }, 5 * 60 * 1000);
+    }
+  }
 }
